45f7b6f15b3ab5f784b78f1f7eef636447717454
BasileDM <xnosx@hotmail.fr>
2025-01-30 16:58:48 +0100

    Add UserService and endpoints

--- src/main/java/com/scorestable/restapi/service/UserService.java
+++ src/main/java/com/scorestable/restapi/service/UserService.java
@@ -1,4 +1,25 @@
 package com.scorestable.restapi.service;
 
+import com.scorestable.restapi.model.User;
+import com.scorestable.restapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
 }
